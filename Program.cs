f89d479e130d85c376773a8e43e5db041485ac3b
Si Lam <silam@hotmail.com>
2023-02-12 18:27:42 -0600

    Clean up

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorStandAloneAADApp;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,15 +10,6 @@ builder.Services.AddScoped(sp => new HttpClient
         { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 
-//builder.Services.AddHttpClient("BlazorWasmWithAADAuth.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-  //     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
-// Supply HttpClient instances that include access tokens when making requests to the server project
-//builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorWasmWithAADAuth.ServerAPI"));
-//if you are trying to get the token for calling another resource you will have to create another ht
-
-
-
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
@@ -27,7 +17,6 @@ builder.Services.AddMsalAuthentication(options =>
         .Add("api://8e2beae8-ce0a-4d93-9101-a093490ffca1/User.Read");
     options.ProviderOptions.LoginMode = "redirect";
 
-    //options.ProviderOptions.DefaultAccessTokenScopes.Add("{SCOPE URI}");
 
 });
 
