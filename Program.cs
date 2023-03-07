b1fe1f72dca0b180872702e8325f8b0e48a9895c
Si Lam <silam@hotmail.com>
2023-03-07 06:50:11 -0600

    App roles

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Authorization;
+
+
 using BlazorStandAloneAADApp;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,10 +13,16 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient 
         { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddIdentityCore<IdentityUser>().AddRoles<IdentityRole>();
 
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+
+   
+
+    //options.UserOptions.RoleClaim = "roles";
+
     options.ProviderOptions.DefaultAccessTokenScopes
         .Add("api://8e2beae8-ce0a-4d93-9101-a093490ffca1/User.Read");
     options.ProviderOptions.LoginMode = "redirect";
